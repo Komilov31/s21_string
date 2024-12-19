d3598872eaf63d1001aa96b6130ce9bd7f6616f2
komil <komilovkomil156@gmail.com>
2024-12-19 19:59:07 +0300

    Some changes

--- src/s21_string.h
+++ src/s21_string.h
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef long unsigned s21_size_t;
+#define s21_NULL (void *)0
 #define s21_NULL (void *)0
 #define s21_error                                                              \
   {                                                                            \
@@ -81,3 +83,11 @@ char *s21_strerror(int errnum);
 char *s21_strtok(char *str, const char *delim);
 char *s21_strerror(int errnum);
 void fill_with_terminate(char *str, const char *delim, int *flag);
+void *s21_memchr(const void *str, int c, s21_size_t n);
+void *s21_memset(void *str, int c, s21_size_t n);
+void *s21_memcpy(void *dest, const void *src, s21_size_t n);
+int s21_memcmp(const void *str1, const void *str2, s21_size_t n);
+s21_size_t s21_strlen(const char *str);
+int s21_strncmp(const char *str1, const char *str2, s21_size_t n);
+char *s21_strchr(const char *str, int c);
+char *s21_strncat(char *dest, const char *src, s21_size_t n);
